29c01b1d46b9e4ec02bec859357e412472e8925e
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-10 01:56:00 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBrNp6k4hF11qgf6mpn7JhfuuUoqVuPETk',
+  authDomain: 'router-firebase-integrat-47f78.firebaseapp.com',
+  projectId: 'router-firebase-integrat-47f78',
+  storageBucket: 'router-firebase-integrat-47f78.appspot.com',
+  messagingSenderId: '783716674729',
+  appId: '1:783716674729:web:fed85ec6a45ebc946a0db8',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
